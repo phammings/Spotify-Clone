d085dbb0c68ba50f1192690ab06ac03610a0db92
Ryan Pham <ryanlepham9@gmail.com>
2024-06-18 13:13:22 -0400

    Authenticate user before clicking add button in library

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,9 +3,17 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
 const Library = () => {
+    const authModal = useAuthModal();
+    const { user } = useUser();
+
     const onClick = () => {
-        
+        if (!user) {
+            return authModal.onOpen();
+        }    
     };
 
     return (
