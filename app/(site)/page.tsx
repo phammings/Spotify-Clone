e32f1b77fdf6408c81621b8d9cd25a41483022f5
Ryan Pham <ryanlepham9@gmail.com>
2024-06-18 19:53:15 -0400

    Fetched song image and display feature

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
-export default function Home() {
+import PageContent from "./components/PageContent";
+
+import getSongs from "@/actions/getSongs";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="
       bg-neutral-900
@@ -47,9 +54,7 @@ export default function Home() {
             Newest songs
           </h1>
         </div>
-        <div>
-          List of Songs!
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
