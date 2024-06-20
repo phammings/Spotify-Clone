e48b52a68a2390be985e843c619a7cf3f66639b3
Ryan Pham <ryanlepham9@gmail.com>
2024-06-19 21:20:54 -0400

    Added player on mediaitem compoenente

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
-// import usePlayer from "@/hooks/usePlayer";
+import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
   data: Song;
@@ -13,13 +13,13 @@ interface MediaItemProps {
 
 const MediaItem: React.FC<MediaItemProps> = ({ onClick, data }) => {
   const imageUrl = useLoadImage(data);
-//   const player = usePlayer();
+  const player = usePlayer();
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id);
     }
-    // return player.setId(data.id);
+    return player.setId(data.id);
   };
 
   return (
